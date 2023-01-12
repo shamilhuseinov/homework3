af22088a98642d89dd41e579f02d63eaa1ed95ea
shamilhuseinov <shamilrh@code.edu.az>
2023-01-13 01:53:17 +0400

    code is added

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿//1
+//int i = 1957;
+//int sum = 0;
+//while (i>0)
+//{
+//    sum += i % 10;
+//    i /= 10;
+//}
+//if (sum%2==0)
+//{
+//    Console.WriteLine("ededin reqemleri cemi cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("ededin reqemleri cemi tekdir");
+//}
+
+//2
+//int i = 100;
+//int sum = 0;
+//while (i > 0)
+//{
+//    if (i % 13 == 0)
+//    {
+//        Console.WriteLine(i);
+//        sum += i;
+//        i--;
+//    }
+//    i--;
+//}
+//Console.WriteLine(sum);
+
+//3
+int i = 4216;
+int k = 9;
+while (i>0)
+{
+    if (k>i%10)
+    {
+        k = i % 10;
+        i /= 10;
+    }
+    else
+    {
+        i /= 10;
+    }
+}
+Console.WriteLine(k);
